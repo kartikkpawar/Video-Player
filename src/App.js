22cce5a28d0299315f72860efb5e355025f488a9
kartikkpawar <knpwar789@gmail.com>
2024-03-01 22:35:39 +0530

    Video player: Static Data

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ const App = () => {
   return (
     <div className="h-screen w-full box-border">
       <div className="flex h-full w-full gap-4 max-w-[1750px] mx-auto p-10 box-border">
-        <div className="flex-[0.7] border-4">
+        <div className="flex-[0.7] border-4 flex flex-col gap-2">
           <VideoPlayer />
+          <div className="flex flex-col gap-1 p-2">
+            <span className="text-2xl line-clamp-2 font-bold">
+              Elephant Dream Elephant Dream
+            </span>
+            <span className="line-clamp-5 leading-5 font-normal text-base">
+              Introducing Chromecast. The easiest way to enjoy online video and
+              music on your TV—for when you want to make Buster's big meltdowns
+              even bigger. For $35. Learn how to use Chromecast with Netflix and
+              more at google.com/chromecast.
+            </span>
+          </div>
         </div>
         <div className="flex-[0.3]">
           <VideoList />
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+const temp_sources = [
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+];
+
 const VideoPlayer = () => {
-  return <div>VideoPlayer</div>;
+  return (
+    <video className="w-full aspect-video" autoPlay muted>
+      {temp_sources.map((source) => (
+        <source src={source} type="video/mp4" />
+      ))}
+    </video>
+  );
 };
 
 export default VideoPlayer;
