465d0acbb4b9a5e483aabd604e81ed97db3f86a8
kartikkpawar <knpwar789@gmail.com>
2024-03-03 22:17:30 +0530

    warning-fixes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     return () => {
       clearTimeout(searchTimeout);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSearch = (e) => {
